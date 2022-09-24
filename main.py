3902e5460b4a412cc69dd853e369d23d9df915e5
student <yaroslavdisiak@gmail.com>
2022-09-24 14:11:29 +0300

    Lab_ex_4

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print(1)
+num1=int(input("Enter number: "))
+num2=int(input("Enter number: "))
+
+# print(f"{a}is more")
+if int (num1>num2):
+    print(f'{num2} is minimum ')
+
+else :
+    print(print(f'{num1} is minimum'))
