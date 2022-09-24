e341ed582721a4dbcb65ead7a977f32e745a62ef
student <yaroslavdisiak@gmail.com>
2022-09-24 14:07:11 +0300

    HM_ex_8

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print(1)
+metrs=int(input("Enter meters: "))
+mili = metrs / 1609
+duim = metrs * 39.37
+yadr = metrs * 1.094
+res=str(input("Enter mil,duim or yadr: "))
+
+# print(f"{a}is more")
+if str(res)=="mil":
+    print(f'{metrs}   is {mili}mil')
+if str(res)=="duim":
+    print(f'{metrs} is {duim}duim')
+if str(res)=="yadr":
+    print(f'{metrs} is {yadr}yard')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
