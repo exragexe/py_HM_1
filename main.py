dce168d4a8b6a54789bf5fa0e7d686a13accd725
student <yaroslavdisiak@gmail.com>
2022-09-24 14:02:21 +0300

    HM_ex_3

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print(1)
+meters=int(input("Enter meters(m): "))
+santimeters=int(meters*100)
+decemeters=int(meters*10)
+milimetr=int(meters*1000)
+milya=float(meters/1609)
+
+print("Sm in m:", santimeters,"\nDcm in m:", decemeters, "\nMiliM in m:", milimetr,"\nMilya in m:", milya)
