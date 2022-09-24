40032f664b90b6b04549487c190752d864a5bf35
student <yaroslavdisiak@gmail.com>
2022-09-24 14:03:59 +0300

    HM_ex_5

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print(1)
+numbers=int(input("Enter numbers: "))
+part1=int(numbers/1000%10)
+part2=int(numbers/100%10)
+part3=int(numbers/10%10)
+part4=int(numbers%10)
+res=part4*1000+part3*100+part2*10+part1
+print(res)
