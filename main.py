015ba0abf6b8f4e883fea9bafffe13b201122d36
student <yaroslavdisiak@gmail.com>
2022-09-24 14:12:02 +0300

    Lab_ex_5

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print(1)
+num1=int(input("Enter number: "))
+num2=int(input("Enter number: "))
+res=str(input("Enter znak: "))
+
+# print(f"{a}is more")
+#if int (num1*num2):
+if str(res)=="*":
+    print(num1*num2)
+elif str(res)=="/":
+    print(num1/num2)
+elif str(res)=="ar":
+    print(num1+num2/2)
+
+
