f206dd8b0bc6fae7c48ec660c43c8cb18d168d01
student <yaroslavdisiak@gmail.com>
2022-09-24 14:05:43 +0300

    HM_ex_6

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print(1)
+num1=int(input("Enter number: "))
+num2=int(input("Enter number: "))
+num3=int(input("Enter number: "))
+res=str(input("Enter znak: "))
+
+# print(f"{a}is more")
+#if int (num1*num2):
+if str(res)=="*":
+    print(num1*num2*num3)
+elif str(res)=="+":
+    print(num1+num2+num3)
