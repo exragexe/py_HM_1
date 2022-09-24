857e1c85bc9c58bdb03dd5bb1f8e045f17cb69ee
student <yaroslavdisiak@gmail.com>
2022-09-24 14:10:56 +0300

    Lab_ex_3

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print(1)
+num1=int(input("Enter number: "))
+num2=int(input("Enter number: "))
+
+# print(f"{a}is more")
+if int (num1>num2):
+    print(f'{num1} is more')
+
+else :
+    print(print(f'{num2} is more'))
