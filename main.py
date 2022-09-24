4bf00f79f8f58568bc49322b911e71004ebbf382
student <yaroslavdisiak@gmail.com>
2022-09-24 14:10:25 +0300

    Lab_ex_2

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print(1)
+num1=int(input("Enter number: "))
+
+# print(f"{a}is more")
+if int(num1 %7)==0:
+    print(f'{num1} Number is multiple 7')
+
+else :
+    print(print(f'{num1} Number is not multiple 7'))
